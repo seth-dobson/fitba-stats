55881ec4f4c394ff158b9bcb1977bef2c38e6728
Seth Dobson <seth.dobson@gmail.com>
2021-06-10 20:13:41 -0700

    create dbt project

---
+++ get_football_data.sh
@@ -0,0 +1,6 @@
+# shell script to download CSV files from football_data.co.uk
+# run in terminal with `sh football_data.sh`
+
+wget -O football_data_scottish_premiership_2020_2021 https://www.football-data.co.uk/mmz4281/2021/SC0.csv
+
+wget https://www.football-data.co.uk/mmz4281/2020/SC0.csv

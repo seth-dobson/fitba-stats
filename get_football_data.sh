5442559ff7ed31d6cdc52d572e3686c77aa2a856
Seth Dobson <seth.dobson@gmail.com>
2021-06-10 22:19:23 -0700

    change shell script

--- get_football_data.sh
+++ get_football_data.sh
@@ -1,6 +1,17 @@
 # shell script to download CSV files from football_data.co.uk
 # run in terminal with `sh football_data.sh`
 
-wget -O football_data_scottish_premiership_2020_2021 https://www.football-data.co.uk/mmz4281/2021/SC0.csv
+wget https://www.football-data.co.uk/mmz4281/2021/SC0.csv
+cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_2020_2021.csv
+mv src_football_data_scottish_premiership_match_results_2020_2021.csv ~/fitba-stats/dbt/data
+rm SC0.csv
 
 wget https://www.football-data.co.uk/mmz4281/2020/SC0.csv
+cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_2019_2020.csv
+mv src_football_data_scottish_premiership_match_results_2019_2020.csv ~/fitba-stats/dbt/data
+rm SC0.csv
+
+wget https://www.football-data.co.uk/mmz4281/2019/SC0.csv
+cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_2018_2019.csv
+mv src_football_data_scottish_premiership_match_results_2018_2019.csv ~/fitba-stats/dbt/data
+rm SC0.csv

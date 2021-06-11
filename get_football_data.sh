58ad4d235548dd9ddca9086172c81f4aa390a1ad
Seth Dobson <seth.dobson@gmail.com>
2021-06-10 22:33:46 -0700

    update seeds and shell script

--- get_football_data.sh
+++ get_football_data.sh
@@ -2,16 +2,16 @@
 # run in terminal with `sh football_data.sh`
 
 wget https://www.football-data.co.uk/mmz4281/2021/SC0.csv
-cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_2020_2021.csv
-mv src_football_data_scottish_premiership_match_results_2020_2021.csv ~/fitba-stats/dbt/data
+cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_2021.csv
+mv src_football_data_scottish_premiership_match_results_2021.csv ~/fitba-stats/dbt/data
 rm SC0.csv
 
-wget https://www.football-data.co.uk/mmz4281/2020/SC0.csv
-cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_2019_2020.csv
-mv src_football_data_scottish_premiership_match_results_2019_2020.csv ~/fitba-stats/dbt/data
+wget https://www.football-data.co.uk/mmz4281/1920/SC0.csv
+cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_1920.csv
+mv src_football_data_scottish_premiership_match_results_1920.csv ~/fitba-stats/dbt/data
 rm SC0.csv
 
-wget https://www.football-data.co.uk/mmz4281/2019/SC0.csv
-cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_2018_2019.csv
-mv src_football_data_scottish_premiership_match_results_2018_2019.csv ~/fitba-stats/dbt/data
+wget https://www.football-data.co.uk/mmz4281/1819/SC0.csv
+cut -d , -f 1-24 SC0.csv > src_football_data_scottish_premiership_match_results_1819.csv
+mv src_football_data_scottish_premiership_match_results_1819.csv ~/fitba-stats/dbt/data
 rm SC0.csv
